4d083fb7e5587a62bac353bed2af8ffb02f690e5
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-20 16:02:48 -0400

    Remove extra console.logs

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,7 @@ import React, {useState} from "react"
 import CategoryFilter from "./CategoryFilter"
 import NewTaskForm from "./NewTaskForm"
 import TaskList from "./TaskList"
-
 import {CATEGORIES, TASKS} from "../data"
-console.log("Here's the data you're working with")
-console.log({CATEGORIES, TASKS})
 
 function App() {
   const [tasks, setTaskList] = useState(TASKS)
