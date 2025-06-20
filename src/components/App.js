52aef8abc3c658239d2acc425449631c445cfc2d
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-20 13:42:49 -0400

    Display each tasks on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,28 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+import React, {useState} from "react"
+import CategoryFilter from "./CategoryFilter"
+import NewTaskForm from "./NewTaskForm"
+import TaskList from "./TaskList"
 
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import {CATEGORIES, TASKS} from "../data"
+console.log("Here's the data you're working with")
+console.log({CATEGORIES, TASKS})
 
 function App() {
+  const [tasks, SetTaskList] = useState()
+
+  // const taskList = TASKS.map((task) => {
+  //   console.log(task)
+  // })
+  // console.log(taskList)
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={TASKS} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from "react"
 
-function Task() {
+function Task({text, category}) {
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
       <button className="delete">X</button>
     </div>
-  );
+  )
 }
 
-export default Task;
+export default Task
