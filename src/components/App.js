3c8ce87cc0b3dcead5ded27a4bf29d3ec54721c0
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-20 14:09:38 -0400

    Pass list of categories and create button for each one on page

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={CATEGORIES} />
       <NewTaskForm />
       <TaskList tasks={tasks} deleteTask={deleteTask} />
     </div>
